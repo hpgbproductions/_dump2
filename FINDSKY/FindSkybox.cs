0aa58881e0b88ae9e1e0ee3ad8fef6c2c21851aa
hpgbproductions <36079650+hpgbproductions@users.noreply.github.com>
2021-03-21 00:30:41 +0800

    Create FindSkybox.cs

---
+++ FINDSKY/FindSkybox.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class DarkBlue : MonoBehaviour
+{
+    private void Start()
+    {
+        ServiceProvider.Instance.DevConsole.RegisterCommand("FindSkyDome", FindSkyDome);
+    }
+
+    private void FindSkyDome()
+    {
+        GameObject sky = GameObject.Find("SkyDome_Low(Clone)");
+
+        if (sky == null)
+        {
+            Debug.LogError("SkyDome not found");
+        }
+        else
+        {
+            Component[] components = sky.GetComponents<Component>();
+            
+            for (int i = 0; i < components.Length; i++)
+            {
+                Debug.Log("SkyDome component found: " + components[i].GetType());
+            }
+        }
+    }
+}
